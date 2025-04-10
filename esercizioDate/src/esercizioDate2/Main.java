6f853aa722161ca1aeab2c48aa460b6b9dc4ebfa
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-04-10 17:52:11 +0200

    Esercizio date2

---
+++ esercizioDate/src/esercizioDate2/Main.java
@@ -0,0 +1,27 @@
+package esercizioDate2;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //stringa base
+        String input = "2023-03-01T13:00:00Z";
+        //oggetto OffsetDateTime
+        OffsetDateTime dataOffset = OffsetDateTime.parse(input);
+        //formato richiesto
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy",Locale.ITALIAN);
+        //Stampa
+        System.out.println(formatter.format(dataOffset));
+    }
+
+}
+
+/*
+Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+Formatta la data ottenendo 01 marzo 2023
+Stampa sulla console
+ */
