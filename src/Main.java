7c3f85c9d207c5dd9b44eac9a91df8bb476035e0
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-05-04 19:52:13 +0200

    pulizia branch testing , in modo che adesso quando passo ad un'altro branch per il prossimo esercizio, riparto con il progetto vuoto

--- src/Main.java
+++ src/Main.java
@@ -8,39 +8,10 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        //creazione data
-        ZonedDateTime data = creaData();
-        String dataModificata = dataModificata(data);
-
-        //stampe
-        System.out.println(data);
-        System.out.println(dataModificata);
-
     }
-
-    public static ZonedDateTime creaData(){
-        String dataStringa = "2023-03-01T13:00:00Z";
-        return ZonedDateTime.parse(dataStringa);
-    }
-
-    public static String dataModificata(ZonedDateTime dataOffset){
-        ZonedDateTime risultato = dataOffset.plusYears(1).minusMonths(1).plusDays(7);
-        // Formatter localizzato per l’Italia
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)
-                .withLocale(Locale.ITALY);
-        return formatter.format(risultato);
-    }
-
 
 }
 
 /*
-Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
-aggiungi un anno
-sottrai un mese
-aggiungi 7 giorni
-Stampa il risultato localizzata per l'Italia
-Crea dei test per questo esercizio
-
 
  */
--- test/MainTest.java
+++ test/MainTest.java
@@ -9,20 +9,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
-    void creaData() {
-        String dataStringa = "2023-03-01T13:00:00Z";
-        ZonedDateTime data = ZonedDateTime.parse(dataStringa);
-        assertEquals(data,Main.creaData());
-    }
-
-    @Test
-    void dataModificata() {
-        String dataStringa = "2023-03-01T13:00:00Z";
-        ZonedDateTime data = ZonedDateTime.parse(dataStringa);
-        ZonedDateTime risultato = data.plusYears(1).minusMonths(1).plusDays(7);
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)
-                .withLocale(Locale.ITALY);
-        assertEquals(formatter.format(risultato),Main.dataModificata(data));
-    }
 }
